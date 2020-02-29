a7105bb6822273059e982f9b2feca00a3828e23c
zhangxiaohuan8 <2916244782@qq.com>
2020-02-29 13:35:40 +0800

    add

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,22 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    run: {
+      api: {
+        options: { wait: false },
+        args: ['./app.js']
+      }
+    },
     mochacli: {
       options: {
         reporter: 'spec',
         bail: true
       },
-      all: ['test/*.js']
-    },
-    mocha_istanbul: {
-      coverage: {
-        src: 'test'
-      }
-    },
-    istanbul_check_coverage: {
-      default: {
-        options: {
-          coverageFolder: 'coverage*',
-          check: {
-            lines: 90,
-            statements: 90
-          }
-        }
-      }
+        all: ['test/*.js']
     }
   });
 
-  grunt.loadNpmTasks('grunt-mocha-cli');
-  grunt.loadNpmTasks('grunt-mocha-istanbul');
+  grunt.loadNpmTasks('grunt-run')
+  grunt.loadNpmTasks('grunt-mocha-cli')
 
-  grunt.registerTask('default', ['mochacli']);   
-  grunt.registerTask('cover', ['mocha_istanbul']);
-  grunt.registerTask('check-cover', ['istanbul_check_coverage']);
+  grunt.registerTask('default', ['run', 'mochacli', 'stop:api']);
 };
